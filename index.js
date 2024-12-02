4b11950663194fe8a880dc57799262c847a977a8
Piyush Goyal <piyush2022ug@gmail.com>
2024-12-02 13:40:49 +0530

    fix: installed cors

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 initialiseDatabse();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
+
+
 async function createBook(newBook) {
   try {
     const book = new Books(newBook);
