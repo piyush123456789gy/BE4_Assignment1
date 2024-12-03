8ae02db5b99274ef5e9ff94e175abab39509dabf
piyush123456789gy <168422795+piyush123456789gy@users.noreply.github.com>
2024-12-03 09:21:17 +0530

    frontend issue

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ initialiseDatabse();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:5173/",
+  origin: "http://localhost:5173",
   credentials: true,
   optionSuccessStatus: 200,
 };
