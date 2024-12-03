726eb45ac00442d189e014296dcd823c50df6249
piyush123456789gy <168422795+piyush123456789gy@users.noreply.github.com>
2024-12-03 09:12:06 +0530

    Update index.js for frontend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ initialiseDatabse();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173/",
   credentials: true,
   optionSuccessStatus: 200,
 };
